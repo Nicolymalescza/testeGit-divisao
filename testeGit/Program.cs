2874f4288043c2c4cf0d656d9b56333e3d957981
Nicoly <nicolysilvamalescza@gmail.com>
2023-05-30 15:03:33 -0400

    Divisão simples para (C#) teste do GitHub

---
+++ testeGit/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("informe dois numeros: ");
+double a = Convert.ToInt32(Console.ReadLine());
+double b = Convert.ToInt32(Console.ReadLine());
+double conta = a / b;
+Console.WriteLine(conta);
+Console.ReadKey();
